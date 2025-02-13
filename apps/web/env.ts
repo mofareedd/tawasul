a29dd9d89c7cccb8464c3fec2ffaafa9e58cc4b3
mofareedd <m.fareeed1997@gmail.com>
2025-02-13 04:23:07 +0300

    fix(web): resend api key

--- apps/web/env.ts
+++ apps/web/env.ts
@@ -1,10 +1,9 @@
 import { createEnv } from '@t3-oss/env-nextjs';
 import { keys as auth } from '@tawasul/auth/keys';
 import { keys as db } from '@tawasul/db/keys';
-import { keys as email } from '@tawasul/email/keys';
 
 export const env = createEnv({
-  extends: [auth(), db(), email()],
+  extends: [auth(), db()],
   server: {},
   client: {},
   runtimeEnv: {},
